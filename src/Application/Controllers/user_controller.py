87b879971e26ee932ecf83cc2a9f43597eed829f
JoaoVictor2201 <franca.jv@hotmail.com>
2026-03-19 18:05:04 -0300

    remove Twilio

--- src/Application/Controllers/user_controller.py
+++ src/Application/Controllers/user_controller.py
@@ -1,33 +1,35 @@
-import code
+import random
 
 from flask import request, jsonify, make_response
 from src.Application.Service.user_service import UserService
-from src.Infrastructure.http.whats_app import WhatsAppService
 
 class UserController:
     @staticmethod
     def register_user():
-        data = request.get_json()
+        try:
+            data = request.get_json()
 
-        name = data.get('nome')
-        cnpj = data.get('cnpj')
-        email = data.get('email')
-        celular = data.get('celular')
-        password = data.get('senha')
+            name = data.get('nome')
+            cnpj = data.get('cnpj')
+            email = data.get('email')
+            celular = data.get('celular')
+            password = data.get('senha')
 
-        if not name or not email or not password:
-            return make_response(jsonify({"erro": "Missing required fields"}), 400)
+            if not name or not email or not password:
+                return make_response(jsonify({"erro": "Missing required fields"}), 400)
 
-        code = WhatsAppService.generate_code()
+            code = str(random.randint(1000, 9999))
+            print(f'Código de ativação gerado: {code}')
 
-        user = UserService.create_user(name, cnpj, email, celular, password, code)
-        
-        WhatsAppService().send_activation_message(celular, code)
+            user = UserService.create_user(name, cnpj, email, celular, password, code)
 
-        return make_response(jsonify({
-            "mensagem": "User salvo com sucesso",
-            "usuarios": user.to_dict()
-        }), 201)
+            return make_response(jsonify({
+                "mensagem": "User salvo com sucesso",
+                "usuarios": user.to_dict()
+            }), 201)
+        except Exception as e:
+            print(f"Error registering user: {e}")
+            return make_response(jsonify({"erro": f"An error occurred: {e}"}), 500)
     @staticmethod
     def activate_user():
         data = request.get_json()
