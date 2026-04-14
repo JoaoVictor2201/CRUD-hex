eaa8a28fdeb8bf64ea85b27ccd66b35b65a5609d
AnaLaurabsz <analaura.b.souza1@gmail.com>
2026-04-14 20:16:36 -0300

    remove comments

--- src/Application/Controllers/product_controller.py
+++ src/Application/Controllers/product_controller.py
@@ -1,8 +1,6 @@
 from flask import request, jsonify, make_response
 from src.Application.Service.product_service import ProductService
-# import code
-# import jwt
-# import os
+
 
 class ProductController:
     @staticmethod
@@ -22,18 +20,14 @@ class ProductController:
         quantity = data.get('quantity')
         image = data.get('image')
 
-        # Validação de campos em branco
         if not name or not price or not quantity or not image:
             return make_response(jsonify({"erro": "Missing required fields"}), 400)
  
-        # Chama o Service
         product = ProductService.create_product(name, price, quantity, image)
  
-        # Checa se a validação do banco barrou (Produto já existe)
         if not product["success"]:
             return make_response(jsonify({"erro": product["message"]}), 400)
  
-        # Caminho Feliz: Pega o produto de dentro do dicionário
         productDomain = product["produto"]
 
         return make_response(
